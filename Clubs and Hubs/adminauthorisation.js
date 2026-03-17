86016db979351e99d52fc837c290a6537a34b01e
KowshikRaj R <kowshikraja1@gmail.com>
2026-03-17 11:58:44 +0530

    Delete Clubs and Hubs directory

--- Clubs and Hubs/adminauthorisation.js
+++
@@ -1,5 +0,0 @@
-const isLoggedIn = localStorage.getItem("isLoggedIn");
-const role = localStorage.getItem("role");
-if (isLoggedIn !== "true" || role !== "admin") {
-    window.location.href = "home.html";
-}
